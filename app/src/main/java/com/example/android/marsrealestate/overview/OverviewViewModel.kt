cf53b55ac62d8ba02b94bd21b2bbb16011ecc3ef
oscar <oscar.goodwin@gmail.com>
2021-10-18 12:16:52 +0800

    Display property of downloaded string into a text box

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -25,10 +25,6 @@ import com.example.android.marsrealestate.network.MarsApi
 import com.example.android.marsrealestate.network.MarsProperty
 import kotlinx.coroutines.launch
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -36,11 +32,20 @@ import retrofit2.Response
 class OverviewViewModel : ViewModel() {
 
     // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+    private val _status = MutableLiveData<String>()
 
     // The external immutable LiveData for the request status String
     val response: LiveData<String>
-        get() = _response
+        get() = _status
+
+    //Internet property
+    private val _property = MutableLiveData<MarsProperty>()
+
+    //External property
+    val property: LiveData<MarsProperty>
+        get() = _property
+
+
 
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
@@ -56,9 +61,11 @@ class OverviewViewModel : ViewModel() {
         viewModelScope.launch {
             try {
                 var listResult = MarsApi.retrofitService.getProperties()
-                _response.value = "Success: ${listResult.size} Mars properties retrieved"
+                if (listResult.size > 0) {
+                    _property.value = listResult[0]
+                }
             } catch (e: Exception) {
-                _response.value = "Failure: ${e.message}"
+                _status.value = "Failure: ${e.message}"
             }
         }
     }
