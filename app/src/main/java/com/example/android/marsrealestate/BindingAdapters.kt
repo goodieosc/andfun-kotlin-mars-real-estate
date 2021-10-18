7d6ba1c39e9783574e49794bd49942470cd69f9f
oscar <oscar.goodwin@gmail.com>
2021-10-18 12:27:53 +0800

    display an image while your image downloads, and an error image in case it can't be retrieved.

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 //create a BindingAdapter to convert imgUrl to a URI with the https scheme.
 @BindingAdapter("imageUrl")
@@ -12,6 +13,12 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(imgUri)
+            //apply RequestOptions to the Glide call to add a placeholder that display
+            // an image while your image downloads, and an error image in case it can't be retrieved.
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
             .into(imgView)
     }
 }
